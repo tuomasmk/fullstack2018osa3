febe6facbd7aaeed6a5987d4f40859cba2aa51c5
Tuomas Konttinen <32363849+tuomasmk@users.noreply.github.com>
2018-02-08 22:05:57 +0200

    osa 3.3

--- index.js
+++ index.js
@@ -15,6 +15,17 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if ( person ) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log('Server running on port ${PORT}')
