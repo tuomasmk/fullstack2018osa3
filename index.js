d30a32371aad11bd12a1e1e03f786a6a9d477dbd
Tuomas Konttinen <32363849+tuomasmk@users.noreply.github.com>
2018-02-08 22:12:54 +0200

    osa 3.4

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
-app.get('/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = persons.find(person => person.id === id)
 
@@ -24,6 +24,13 @@ app.get('/persons/:id', (req, res) => {
     } else {
         res.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
