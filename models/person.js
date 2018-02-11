93b12d92014f585e32686aec55b55a5cebe78ad2
Tuomas Konttinen <32363849+tuomasmk@users.noreply.github.com>
2018-02-11 22:58:21 +0200

    add person.js

---
+++ models/person.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+if( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
+const url = process.env.MONGODB_URI
+
+// korvaa url oman tietokantasi urlilla. ethän laita salasanaa Gothubiin!
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+personSchema.statics.filter = function (person) {
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  };
+}
+
+const Person = mongoose.model('Person', personSchema);
+
+module.exports = Person
